588afea835f593f689b8adb26474a4bf0bce6cc4
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-08-20 11:11:45 +0300

    Update_1 project

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Counter = () => {
-    return <h1>Counter</h1>;
+	const [count, setCount] = useState(0);
+	const tags = ['tag1', 'tag2', 'tag3'];
+
+	const formCount = () => (count === 0 ? 'Ноль' : count);
+
+	const getBadgeClasses = () => {
+		let classes = 'badge m-2 bg-';
+		classes += count === 0 ? 'danger' : 'primary';
+		return classes;
+	};
+
+	const renderTags = () => {
+		if (tags.length === 0) return 'Тегов не найдено';
+		return tags.map(tag => <li key={tag}>{tag}</li>);
+	};
+
+	const handleIncrement = (productId) => {
+		console.log('productId:', productId);
+		setCount(count + 1);
+	}
+
+	const handleDecrement = (productId) => {
+		if (count === 0) return;
+		console.log('productId:', productId);
+		setCount(count - 1);
+	}
+
+	return (
+		<>
+			{renderTags()}
+			<span className={getBadgeClasses()}>{formCount()}</span>
+			<button
+				onClick={() => handleIncrement({id: 1})}
+				className="btn btn-secondary btn-sm">Increment
+			</button>
+			<button
+				onClick={() => handleDecrement({id: 1})}
+				className="btn btn-secondary btn-sm">Decrement
+			</button>
+		</>
+	);
 };
 
 export default Counter;
